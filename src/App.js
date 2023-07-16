6edcf953585c2ad122498ee763321cc3950327e4
Jana <prikrylovajana003@gmail.com>
2023-07-16 14:21:07 +0200

    Rendering the Item List

--- src/App.js
+++ src/App.js
@@ -1,3 +1,9 @@
+const initialItems = [
+  { id: 1, description: 'Passports', quantity: 2, packed: false },
+  { id: 2, description: 'Socks', quantity: 12, packed: true },
+  { id: 3, description: 'Charger', quantity: 1, packed: false },
+];
+
 export default function App() {
   return (
     <div className="app">
@@ -16,13 +22,32 @@ function Logo() {
 function Form() {
   return (
     <div className="add-form">
-      <h3>What do you need for your 😍 trip</h3>
+      <h3>What do you need for your 😍 trip?</h3>
     </div>
   );
 }
 
 function PackingList() {
-  return <div className="list">LIST</div>;
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((item) => (
+          <Item item={item} />
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+function Item({ item }) {
+  return (
+    <li>
+      <span style={item.packed ? { textDecoration: 'line-through' } : {}}>
+        {item.quantity} {item.description}
+      </span>
+      <button>❌</button>
+    </li>
+  );
 }
 
 function Stats() {
